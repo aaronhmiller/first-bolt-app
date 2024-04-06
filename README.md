3c4ab369f32296ebd6707103871867de4fe60626
Aaron Miller <aaronmiller@gmail.com>
2024-04-06 08:51:26 -0700

    seeding repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# first-bolt-app
-Learning how to create Slack Apps
+### ngrok advice
+Startup ngrok on port 3000 b/c that's where the Slackbot is running
+ngrok http --domain=unbiased-dane-closely.ngrok-free.app 3000
+
+### setting up views
+`https POST slack.com/api/views.publish -A bearer -a SLACK_BOT_TOKEN < home-view.json`
---
+++ app.js
@@ -0,0 +1,104 @@
+const { App, LogLevel } = require('@slack/bolt');
+const { config } = require('dotenv');
+
+config();
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel: LogLevel.DEBUG,
+
+  //Respond to Event challenge
+  customRoutes: [
+    {
+      path: '/slack/events',
+      method: ['POST'],
+      handler: (req, res) => {
+        res.writeHead(200);
+        res.end(`${req.body.challenge}`);
+      },
+    },
+  ],
+
+
+
+
+});
+
+
+
+
+app.event('app_home_opened', async ({ event, client, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await client.views.publish({
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home tab_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+
+
+
+
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there via HTTP <@${message.user}>!`);
+});
+
+
+(async () => {
+  try {
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running! ⚡️');
+  } catch (error) {
+    console.error('Unable to start App', error);
+  }
+})();
